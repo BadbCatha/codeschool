8c054a6dd0d19aee0228f81178c91a25db891c8f
BadbCatha <inia.em@gmail.com>
2017-12-02 13:29:38 +0100

    display last post from database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,13 @@ class ArticlesController < ApplicationController
  	article_params = params.require(:article).permit(:title, :text)
  	@article = Article.new(article_params)
  	@article.save
- 	redirect_to articles_path
+ 	redirect_to article_path(@article)
  end
+
+
+ def show
+ 	@article =  Article.find(params[:id])
+ 	
+ end
+
 end
