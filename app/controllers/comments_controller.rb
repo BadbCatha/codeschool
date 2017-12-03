6c263f9fc5f2e5489501d6aa0661a602f0db7c65
BadbCatha <inia.em@gmail.com>
2017-12-03 14:58:25 +0100

    Comments code optilalize2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ActionController::Base
-	def create
-		@comment = Comment.new(comment_params)
-		@comment.save
-		redirect_to article_path(@comment.article_id)
-		
- 	end
+def create
+    @article = Article.find(params[:article_id])
+    #comment_params = params.require(:comment).permit(:commenter, :body)
+    @comment = Comment.new(article_id: @article.id)
+    @comment.attributes = comment_params
+    @comment.save
+    redirect_to article_path(@comment.article_id)
+  end
 
 private
 	def comment_params 
